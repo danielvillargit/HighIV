fe6c8902382629f49f4b95b4adcb317db95e6eac
danielvillargit <60103934+danielvillargit@users.noreply.github.com>
2020-08-18 11:41:17 -0500

    Added instance of Class OptionSearch

---
+++ MainOption.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 18 00:20:40 2020
+
+@author: Daniel
+"""
+
+!pip install selenium
+from selenium_barchart_2 import *
+tc=OptionSearch()
+#tc.BarchartImport()
+tc.OptionImport()
+tc.AmerConnect()
+
