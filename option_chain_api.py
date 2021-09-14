590c136e8384e3a8da514baf3d0bc7fa8c949bbc
Daniel Villar <dvillarmo@gmail.com>
2021-09-14 15:10:18 -0500

    Adding current price for getting market quotes from YahooFinancials API

--- option_chain_api.py
+++ option_chain_api.py
@@ -7,6 +7,7 @@ Created on Mon Sep 13 23:05:57 2021
 
 import requests
 import pandas as pd
+from yahoofinancials import YahooFinancials as yf
 
 ticker = 'SPY'
 toke = 'H4WiYj0CVcFpz7hZeFpj9uxKJxcJ'
@@ -23,29 +24,26 @@ print(res.status_code)
 print(json_response)
 
 
-#res2 returns response200
-res2 = requests.get('https://sandbox.tradier.com/v1/markets/quotes',
-                   params={'symbols': ticker, 'greeks': 'false'},
-                   headers={'Authorization': btoke, 'Accept': 'application/json'})
-
-
-res2.json()
-print(res2.status_code)
-print(res2)
+#ideally best not to call from 2 api's but tradier not working
+yf1 = yf(ticker)
+ticker_price = yf1.get_current_price()
 
 
 
 
 
-listy_ = ['symbol','description','strike','root_symbol','option_type','bid','ask','open_interest','volume']
+
+
+
 
 
 
 #bid,ask,symbol,description,volume,open_interest,option_type,root_symbol,strike
 #define expiry dates, convert dict to dataframe
 
+listy_ = ['symbol','description','strike','root_symbol','option_type','bid','ask','open_interest','volume']
+
 nest1 = json_response['options']
 nest2 = nest1['option']
 x = pd.DataFrame.from_dict(nest2)
-
 x = x[x.columns.intersection(listy_)]
